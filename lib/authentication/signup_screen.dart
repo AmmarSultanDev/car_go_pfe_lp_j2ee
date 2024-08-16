44e5f3efb5a622975a17797a2746afdf541f71b2
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-08-17 00:38:54 +0100

    chore: Add Roboto and Montserrat fonts, update theme colors based on the device settings.

--- lib/authentication/signup_screen.dart
+++ lib/authentication/signup_screen.dart
@@ -1,3 +1,4 @@
+import 'package:car_go_pfe_lp_j2ee/resources/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -8,17 +9,89 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Image.asset('assets/images/logo_white_borders.png',
-                  height: 200, width: 200),
-            ],
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 32,
+        ),
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ConstrainedBox(
+              constraints:
+                  BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 50),
+                  Image.asset('assets/images/logo_white_borders.png',
+                      height: 200, width: 200),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Create a User\'s Account',
+                  ),
+                  // text fields
+                  Padding(
+                    padding: const EdgeInsets.all(22),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _usernameController,
+                          keyboardType: TextInputType.text,
+                          decoration: const InputDecoration(
+                            labelText: 'Username',
+                            hintText: 'Enter your username',
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            hintText: 'Enter your email',
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: _passwordController,
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                            hintText: 'Enter your password',
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: _confirmPasswordController,
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: 'Confirm password',
+                            hintText: 'Confirm your password',
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Sign Up',
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,54 @@
 import 'package:car_go_pfe_lp_j2ee/authentication/signup_screen.dart';
+import 'package:car_go_pfe_lp_j2ee/resources/app_colors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Colors.black,
+      title: 'My App',
+      theme: ThemeData(
+        primaryColor: AppColors.lightPrimary,
+        scaffoldBackgroundColor: AppColors.lightSurface,
+        colorScheme: const ColorScheme.light(
+          primary: AppColors.lightPrimary,
+          onPrimary: AppColors.lightOnPrimary,
+          secondary: AppColors.lightSecondary,
+          onSecondary: AppColors.lightOnSecondary,
+        ),
+        fontFamily: 'Montserrat',
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headlineMedium:
+              TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          headlineSmall: TextStyle(fontSize: 14.0, fontFamily: 'Roboto'),
+        ),
       ),
-      home: SignupScreen(),
+      darkTheme: ThemeData(
+        primaryColor: AppColors.darkPrimary,
+        canvasColor: AppColors.darkBackground,
+        scaffoldBackgroundColor: AppColors.darkSurface,
+        colorScheme: const ColorScheme.dark(
+          primary: AppColors.darkPrimary,
+          onPrimary: AppColors.darkOnPrimary,
+          secondary: AppColors.darkSecondary,
+          onSecondary: AppColors.darkOnSecondary,
+        ),
+        fontFamily: 'Georgia',
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headlineMedium:
+              TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          headlineSmall: TextStyle(fontSize: 14.0, fontFamily: 'Roboto'),
+        ),
+      ),
+      themeMode: ThemeMode
+          .system, // Automatically select the theme based on the system settings
+      home: const SignupScreen(),
     );
   }
 }
