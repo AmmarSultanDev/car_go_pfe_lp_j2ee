fc42dcea97270a474f7c7c7b7a1ce57214df1c1f
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-08-17 13:29:37 +0100

    chore: Update dependencies and configurations for iOS and Windows platforms, configurate project to use firebase features and dependecies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:car_go_pfe_lp_j2ee/authentication/signin_screen.dart';
+import 'package:car_go_pfe_lp_j2ee/firebase_options.dart';
 import 'package:car_go_pfe_lp_j2ee/resources/app_colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
